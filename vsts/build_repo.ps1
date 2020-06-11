cc53cc2ce1d323f0b1779eeea94e013b490b94bc
timtay-microsoft <timtay@microsoft.com>
2020-06-11 16:03:03 -0700

    skip javadocs for windows/linux testing

--- vsts/build_repo.ps1
+++ vsts/build_repo.ps1
@@ -1,3 +1,3 @@
 ﻿./vsts/determine_tests_to_run.ps1
 
-mvn -DRUN_PROVISIONING_TESTS="$Env:runProvisioningTests" -DRUN_IOTHUB_TESTS="$Env:runIotHubTests" install -DskipITs=false -T 2C "-Dfailsafe.rerunFailingTestsCount=2" #parallelized to use 2 threads per core
+mvn -DRUN_PROVISIONING_TESTS="$Env:runProvisioningTests" -DRUN_IOTHUB_TESTS="$Env:runIotHubTests" install -DskipITs=false -Dmaven.javadoc.skip=true -T 2C "-Dfailsafe.rerunFailingTestsCount=2" #parallelized to use 2 threads per core
