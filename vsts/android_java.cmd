8098cc679a100c37212af09fd1b94572ba6534c8
timtay-microsoft <timtay@microsoft.com>
2020-06-11 18:40:59 -0700

    asdf

--- vsts/android_java.cmd
+++ vsts/android_java.cmd
@@ -13,7 +13,7 @@ call RD /S /Q "c:/users/%USERNAME%/.m2/repository/com/microsoft/azure/sdk/iot"
 
 @REM -- Android Test Build --
 cd %build-root%
-call mvn clean install -Dmaven.javadoc.skip=true -DskipTests -T 100 --projects com.microsoft.azure.sdk.iot:iot-e2e-common,com.microsoft.azure.sdk.iot:iot-deps,com.microsoft.azure.sdk.iot:iot-device-client,com.microsoft.azure.sdk.iot:iot-service-client,com.microsoft.azure.sdk.iot.provisioning:provisioning-device-client,com.microsoft.azure.sdk.iot.provisioning:provisioning-service-client,com.microsoft.azure.sdk.iot.provisioning.security:security-provider,com.microsoft.azure.sdk.iot.provisioning.security:tpm-provider,com.microsoft.azure.sdk.iot.provisioning.security:x509-provider,com.microsoft.azure.sdk.iot.provisioning.security:tpm-provider-emulator,com.microsoft.azure.sdk.iot.provisioning.security:dice-provider-emulator
+call mvn clean install -Dmaven.javadoc.skip=true -DskipTests -T 2C --projects com.microsoft.azure.sdk.iot:iot-e2e-common,com.microsoft.azure.sdk.iot:iot-deps,com.microsoft.azure.sdk.iot:iot-device-client,com.microsoft.azure.sdk.iot:iot-service-client,com.microsoft.azure.sdk.iot.provisioning:provisioning-device-client,com.microsoft.azure.sdk.iot.provisioning:provisioning-service-client,com.microsoft.azure.sdk.iot.provisioning.security:security-provider,com.microsoft.azure.sdk.iot.provisioning.security:tpm-provider,com.microsoft.azure.sdk.iot.provisioning.security:x509-provider,com.microsoft.azure.sdk.iot.provisioning.security:tpm-provider-emulator,com.microsoft.azure.sdk.iot.provisioning.security:dice-provider-emulator
 if errorlevel 1 goto :eof
 cd %build-root%\iot-e2e-tests\android
 call gradle wrapper
